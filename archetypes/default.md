381c13a464665c6df43adc32b3d1cfa7a1cf1455
Raymond Camden <Raymond Camden>
2016-10-05 15:58:42 -0500

    new

--- archetypes/default.md
+++
@@ -1,8 +0,0 @@
-{
-	"title": "",
-	"categories": [
-		"Uncategorized"
-	],
-	"tags": [],
-	"url": "something"
-}
--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Running Hugo..."
-hugo
-echo "Running Surge..."
-surge -p /Users/raymondcamden/Dropbox/websites/2016.raymondcamden.com/hugo_dynamic/public
